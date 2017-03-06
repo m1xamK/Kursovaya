5a49f41141cebd91a0ed12ea9fd9d7c178f78eb3
m1xamk <m1xam1@yandex.ru>
2017-03-06 23:34:36 +0300

    Добавлены Load/Save, random для начальных положений

--- SZ40/SZ40/Form1.Designer.cs
+++ SZ40/SZ40/Form1.Designer.cs
@@ -28,107 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.save1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.gToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tOOLSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.chooseLongtermKeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // save1
             // 
-            this.button1.Location = new System.Drawing.Point(423, 30);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 35);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.save1.Location = new System.Drawing.Point(313, 439);
+            this.save1.Margin = new System.Windows.Forms.Padding(4);
+            this.save1.Name = "save1";
+            this.save1.Size = new System.Drawing.Size(147, 43);
+            this.save1.TabIndex = 0;
+            this.save1.Text = "Save";
+            this.save1.UseVisualStyleBackColor = true;
+            this.save1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(27, 81);
+            this.textBox1.Location = new System.Drawing.Point(36, 100);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(319, 270);
+            this.textBox1.Size = new System.Drawing.Size(424, 331);
             this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(392, 81);
+            this.textBox2.Location = new System.Drawing.Point(523, 100);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(319, 270);
+            this.textBox2.Size = new System.Drawing.Size(424, 331);
             this.textBox2.TabIndex = 4;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(574, 30);
+            this.button2.Location = new System.Drawing.Point(134, 439);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(110, 35);
+            this.button2.Size = new System.Drawing.Size(147, 43);
             this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
+            this.button2.Text = "Load";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // maskedTextBox1
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(40, 27);
+            this.maskedTextBox1.Location = new System.Drawing.Point(36, 31);
+            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(4);
             this.maskedTextBox1.Mask = "00000";
             this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.Size = new System.Drawing.Size(63, 22);
             this.maskedTextBox1.TabIndex = 6;
             this.maskedTextBox1.ValidatingType = typeof(int);
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(176, 15);
+            this.label1.Location = new System.Drawing.Point(131, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(94, 17);
             this.label1.TabIndex = 7;
-            this.label1.Text = "label1";
+            this.label1.Text = "long-term key";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // maskedTextBox2
             // 
-            this.maskedTextBox2.Location = new System.Drawing.Point(40, 55);
-            this.maskedTextBox2.Mask = "00 00 00 00 00 00 00 00 00 00 00 00";
+            this.maskedTextBox2.Location = new System.Drawing.Point(36, 63);
+            this.maskedTextBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.maskedTextBox2.Mask = "000000000000";
             this.maskedTextBox2.Name = "maskedTextBox2";
-            this.maskedTextBox2.Size = new System.Drawing.Size(190, 20);
+            this.maskedTextBox2.Size = new System.Drawing.Size(102, 22);
             this.maskedTextBox2.TabIndex = 9;
-            // 
-            // linkLabel1
-            // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(260, 48);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
-            this.linkLabel1.TabIndex = 10;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "linkLabel1";
+            this.maskedTextBox2.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox2_MaskInputRejected);
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.gToolStripMenuItem,
             this.tOOLSToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(738, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(984, 28);
             this.menuStrip1.TabIndex = 11;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // gToolStripMenuItem
             // 
             this.gToolStripMenuItem.Name = "gToolStripMenuItem";
-            this.gToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.gToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
             this.gToolStripMenuItem.Text = "FILE";
             // 
             // tOOLSToolStripMenuItem
@@ -136,30 +143,42 @@
             this.tOOLSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.chooseLongtermKeyToolStripMenuItem});
             this.tOOLSToolStripMenuItem.Name = "tOOLSToolStripMenuItem";
-            this.tOOLSToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.tOOLSToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
             this.tOOLSToolStripMenuItem.Text = "TOOLS";
             // 
             // chooseLongtermKeyToolStripMenuItem
             // 
             this.chooseLongtermKeyToolStripMenuItem.Name = "chooseLongtermKeyToolStripMenuItem";
-            this.chooseLongtermKeyToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.chooseLongtermKeyToolStripMenuItem.Size = new System.Drawing.Size(230, 26);
             this.chooseLongtermKeyToolStripMenuItem.Text = "Choose long-term key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "начальные угловые положения";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(738, 363);
-            this.Controls.Add(this.linkLabel1);
+            this.ClientSize = new System.Drawing.Size(984, 522);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.maskedTextBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.maskedTextBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.save1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
@@ -171,18 +190,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button save1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox2;
-        private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem gToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem tOOLSToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem chooseLongtermKeyToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
 
     }
 }
--- SZ40/SZ40/Form1.cs
+++ SZ40/SZ40/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace SZ40
 {
@@ -17,9 +11,114 @@ namespace SZ40
             InitializeComponent();
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        /// <summary>
+        /// Этот метод берет последний символ из текст бокса и подает его на телетайп.
+        /// P.S.: Если не использовать isKeyDown, то метод зацикливается, реагируя на свои же внесенные изменения. 
+        /// А так только по нажатию клавиши.
+        /// </summary>
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (isKeyDown)
+            {
+                isKeyDown = false;
+                for (int i = this.countSymbols; i < this.textBox1.Text.Length; ++i)
+                {
+                    teletype.Input(textBox1.Text[i]); // Ввод символа в телетайп.
+                }
+
+                textBox1.Text = teletype.GetText(); // Присваиваем строку из телетайпа в текст бокс.
+                textBox1.Select(textBox1.Text.Length, 0); // Установка курсора в конец.
+                this.countSymbols = this.textBox1.Text.Length;                    
+            }
+        }
+
+        /// <summary>
+        /// Реакция на нажатие клавиши.
+        /// </summary>
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            this.isKeyDown = true;
+        }
+
+        private bool isKeyDown;
+
+        private int countSymbols = 0;
+
+        private Teletype teletype = new Teletype();
+
+        /// <summary>
+        /// Сохраняет долговременный ключ, сдвиги и текст из textBox1 в файл.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = ".txt"; // Расширение по умолчанию
+            saveFileDialog.ShowDialog(); // Открывает диалоговое окно
+
+            string path = saveFileDialog.FileName; // Сохраняет путь выбранного файла            
+            if (path == "")
+                return;
+
+            string[] lines = { maskedTextBox1.Text, maskedTextBox2.Text, textBox1.Text }; // Масив содержимого maskedTextBox1 maskedTextBox2 и textBox1
+
+            File.WriteAllLines(path, lines);  // Записывает в файл.
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
         {
 
         }
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Загружает долговременный ключ, сдвиги колес и текст из файла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)  // Load 1
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog(); // Открывает диалоговое окно
+
+            string path = openFileDialog.FileName; // Сохраняет путь выбранного файла
+            if (path == "")
+                return;
+
+            using (StreamReader sr = new StreamReader(path)) // Считывает ключ
+            {
+                maskedTextBox1.Text = sr.ReadLine();
+                maskedTextBox2.Text = sr.ReadLine();
+
+                while (sr.Peek() >= 0)
+                    textBox1.Text += sr.ReadLine() + " ";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void label2_Click(object sender, EventArgs e)
+        {
+            maskedTextBox2.Text = "";
+            Random random = new Random();
+
+            for (int i = 0; i < 6; ++i)
+            {
+                maskedTextBox2.Text += random.Next(4);
+                maskedTextBox2.Text += random.Next(10);
+            }
+        }
+    }
+
+    public partial class Form2 : Form
+    {
     }
 }
