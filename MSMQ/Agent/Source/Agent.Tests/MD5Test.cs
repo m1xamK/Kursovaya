a45f32c505356dec77af94e1589414f1e895a11e
isaev600 <isaev600@yandex.ru>
2017-11-29 18:55:08 +0300

    В нормальном месте теперь лежит + переименовал

--- MSMQ/Agent/Source/Agent.Tests/MD5Test.cs
+++ MSMQ/Agent/Source/Agent.Tests/MD5Test.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Let21.Tests
+namespace Agent.Tests
 {
     using NUnit.Framework;
     [TestFixture]
--- MSMQ/Agent/Source/Agent/MD5.cs
+++ MSMQ/Agent/Source/Agent/MD5.cs
@@ -5,7 +5,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 
-namespace Let21
+namespace Agent
 {
     public static class Agent
     {
