05711dfe5b1266ef5e4bd50ca2e235a270469090
m1xamk <m1xam1@yandex.ru>
2017-11-30 23:25:58 +0300

    1) добавил отрефакторил Agent.cs 2) добавил Manager.cs

---
+++ MSMQ/DistributedPasswordBrute/Manager/Manager.cs
@@ -0,0 +1,44 @@
+﻿using RequestManager;
+
+namespace Manager
+{
+	/// <summary>
+	/// класс осуществляющий равномерное распределение вычисления md5 праобраза 
+	/// </summary>
+	public class Manager
+    {
+		/// <summary>
+		/// некий механизм осуществляющий передачу сообщений до агента
+		/// </summary>
+		private RequestorAdapter _sender;
+
+		/// <summary>
+		/// указываем пути до ресурсов обмена
+		/// </summary>
+		/// <param name="requestResurce"></param>
+		/// <param name="replayResurse"></param>
+		Manager(string requestResurce, string replayResurse)
+		{
+			_sender = new RequestorAdapter(requestResurce, replayResurse);
+		}
+
+		//не ну а чо тут непонятно?????
+		/// <summary>
+		/// отправка сообщения агенту 
+		/// </summary>
+		/// <param name="start"> начальний сдвиг числа от которого пойдет перебор </param>
+		/// <param name="count"> сколько надо посчитать агенту строк после</param>
+		/// <param name="hash">праобразы md5 сверток</param>
+		void Send(string start, int count, string[] hash)
+		{
+			_sender.Send(start, count, hash);
+		}
+
+
+		string GetRange(string startNumber)
+		{
+            // todo;
+		    return "";
+		}
+    }
+}
---
+++ MSMQ/DistributedPasswordBrute/Manager/ScaleOfNotation.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace Manager
+{
+	/// <summary>
+	/// класс реализующий к ичную систему счисления
+	/// </summary>
+	public class ScaleOfNotation
+	{
+		private string _alphabet; //алвавит системы счисления
+		private StringBuilder _value;// зничение
+
+		public ScaleOfNotation(string alphabet, string number = "0")
+		{
+			_alphabet = alphabet;
+			_value = new StringBuilder(number);
+		}
+
+		//реализация опрератора инкремента
+		public static ScaleOfNotation operator++(ScaleOfNotation number)
+		{
+			for (int i = 0; i < number._value.Length - 1; ++i)
+			{
+				int posOfCurentSymbol = number._alphabet.IndexOf(number._value[i]);
+				number._value[i] = number._alphabet[++posOfCurentSymbol%number._alphabet.Length];
+
+				if (number._value[i] != number._alphabet[0])
+					break;
+			}
+		}
+	}
+
+	// класс нужный для сдвига числа на n позиций
+	public static class Notation
+	{
+		public static ScaleOfNotation Shift(this ScaleOfNotation number, int count)
+		{
+			for (int i = 0; i < count; ++i)
+				number++;
+
+			return number;
+		}
+	}
+}
