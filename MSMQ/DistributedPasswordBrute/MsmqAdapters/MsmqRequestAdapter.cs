3d15d2a6b259bd6064a1edabec793fd93d8ce59f
m1xamk <m1xam1@yandex.ru>
2017-12-16 23:52:24 +0300

    update MsmqRequestAdapter!

--- MSMQ/DistributedPasswordBrute/MsmqAdapters/MsmqRequestAdapter.cs
+++ MSMQ/DistributedPasswordBrute/MsmqAdapters/MsmqRequestAdapter.cs
@@ -2,6 +2,9 @@
 using System.Text;
 using Message = System.Messaging.Message;
 
+// ".\private$\ReplyQueue"
+// ".\private$\RequestQueue"
+
 namespace MsmqAdapters
 {
     public class MsmqRequestorAdapter
@@ -9,12 +12,21 @@ namespace MsmqAdapters
         private readonly MessageQueue _requestQueue;
         private readonly MessageQueue _replyQueue;
 
+        /// <summary>
+        /// Инициализирует объект класса MsmqRequestorAdapter
+        /// </summary>
+        /// <param name="requestQueueName">имя очереди запросов</param>
+        /// <param name="replyQueueName">имя очереди ответов</param>
         public MsmqRequestorAdapter(string requestQueueName, string replyQueueName)
         {
             _requestQueue = new MessageQueue(requestQueueName);
             _replyQueue = new MessageQueue(replyQueueName);
 
+            // Фильтр для считывания сообщения со всеми свойствами
             _replyQueue.MessageReadPropertyFilter.SetAll();
+
+            // Задаем формат содержимого сообщения как строку.
+            // ReSharper disable once RedundantExplicitArrayCreation
             ((XmlMessageFormatter)_replyQueue.Formatter).TargetTypeNames = new string[] { "System.String,mscorlib" };
         }
 
@@ -34,18 +46,36 @@ namespace MsmqAdapters
                 msgBody.Append(hash);
             }
             
-            requestMessage.Body = msgBody;
-            requestMessage.ResponseQueue = _replyQueue;
+            requestMessage.Body = msgBody;              // Задаем содержимое сообщения
+            requestMessage.ResponseQueue = _replyQueue; // Задаем обратный адрес
+
+            // Отправляем сообщение
             _requestQueue.Send(requestMessage);
 
-            return requestMessage.Id;
+            // для дебага
+            StringBuilder str = new StringBuilder();
+            str.AppendLine("Sent request message");
+            str.AppendLine("Message ID:" + requestMessage.Id);
+            str.AppendLine("Reply to:" + requestMessage.ResponseQueue.Path);
+            str.AppendLine("Message Body:" + requestMessage.Body);
+
+            return str.ToString();
         }
 
-        public string ReceiveSync() //поменял тут с void на string lenin
+        public string ReceiveSync()
         {
             Message replyMessage = _replyQueue.Receive();
 
-			return null;
+            if (replyMessage == null) 
+                return null;
+
+            StringBuilder info = new StringBuilder();
+            info.AppendLine("Received reply");
+            info.AppendLine("Message ID:" + replyMessage.Id);
+            info.AppendLine("Message Correlation ID:" + replyMessage.CorrelationId);
+            info.AppendLine("Message Body:" + replyMessage.Body);
+
+            return info.ToString();
         }
     }
 }
