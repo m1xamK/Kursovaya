60d35399c886ea12c3889f4b55ea506327473593
MayRunSay <MayRunSay@outlook.com>
2017-12-20 20:50:13 +0300

    Допилил маленьк

--- MSMQ/DistributedPasswordBrute/ManagerRepresentor/ManagerRepresentor.cs
+++ MSMQ/DistributedPasswordBrute/ManagerRepresentor/ManagerRepresentor.cs
@@ -1,88 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Threading;
+
 namespace ManagerRepresentor
 {
     class ManagerRepresentor
     {
 
         private readonly Manager.Manager _manager;
-        private List<string> _hashList;
-        public bool _calcultationFlag;
+        private readonly List<string> _hashList;
+        public bool CalcultationFlag;
         /// <summary>
-        /// Конструктор класса ManagerRepresentor
+        /// Конструктор класса ManagerRepresentor.
         /// </summary>
         /// <param name="requestResource"></param>
         /// <param name="replyResourсe"></param>
         public ManagerRepresentor(string requestResource, string replyResourсe)
         {
-            _hashList = new List<string>(); //инициализация перемееных
-            _calcultationFlag = false;
+            _hashList = new List<string>(); //Инициализация перемееных.
+            CalcultationFlag = false;
             _manager = new Manager.Manager(requestResource, replyResourсe);
         }
         /// <summary>
-        /// Добавляет новый хеш в List
+        /// Добавляет новый хеш в List.
         /// </summary>
         /// <param name="hash">md5 hash summ</param>
         /// <returns></returns>
         public bool PushNewHash(string hash)
         {
-            _hashList.Add(hash); //Добавление md5 в List
+            _hashList.Add(hash); //Добавление md5 в List.
             return true;
         }
         /// <summary>
-        /// Инициализирует процесс вычисления пароля для List'а хеш сумм
+        /// Инициализирует процесс вычисления пароля для List'а хеш сумм.
         /// </summary>
         /// <returns></returns>
-        public bool startCalculation()
+        public bool StartCalculation()
         {
             if (_hashList.Count == 0)
             {
                 Console.WriteLine("Нет хеш сумм для проверки");
                 return false;
             }
-            _calcultationFlag = true;//Установка флага произведения вычислений
-            _manager.FindHash(_hashList.ToArray<string>());//Инициализация вычислений
-
-	        int temp = 0;
-	        
-	        _manager.ReciveSync();
-
-            _hashList.Clear();//После окончания вычислений отчистка _hashList
+            CalcultationFlag = true;//Установка флага произведения вычислений.
+            _manager.FindHash(_hashList.ToArray<string>());//Инициализация вычислений.
+            _manager.ReciveSync();
+            _hashList.Clear();//После окончания вычислений отчистка _hashList.
             return true;
-        }
-
-        /// <summary>
-        /// Функция отвечающая за вывод логов на экран в отдельном потоке
-        /// </summary>
-        /// <returns></returns>
-        public bool PrintResult()
-        {
-            Thread _logthread = new Thread(GetLog);// Создание потока ввыводяшего результат
-            return true;
-        }
-        /// <summary>
-        /// Сама функция выводящая на экран
-        /// </summary>
-        public void GetLog()
-        {
-			string log = "";
-			StreamWriter logStreamWriter = new StreamWriter(@"../logs.txt", true);
-			while (!string.Equals(log, "end"))
-            {
-                //string lastLog = 
-				_manager.ReciveSync();
-				//if (!string.Equals(lastLog, log))
-				//{   
-				//	Console.WriteLine(lastLog);
-				//	logStreamWriter.WriteLine(lastLog);
-				//	log = lastLog;
-				//}
-
-                Thread.Sleep(150);
-            }
         }
     }
 }
---
+++ MSMQ/DistributedPasswordBrute/ManagerRepresentor/ManagerRepresentorConsole.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace ManagerRepresentor
+{
+    class ManagerRepresentorConsole
+    {
+        static void Main()
+        {
+            string[] commandArr = { "" };   //Массив содержит команду и параметры, если такие имеются.
+            Regex regex = new Regex("[0-9a-z]{32}"); //Регулярное выражение проверяющее hash сумму.
+            ManagerRepresentor managerRepresentor = new ManagerRepresentor(".\\Private$\\RequestQueue", ".\\Private$\\ReplyQueue");
+            while (true)
+            {
+                var command = Console.ReadLine();//Считывание команды с параметрами с консоли
+                if (command != null) commandArr = command.Split((string[]) null, StringSplitOptions.RemoveEmptyEntries);//Если command существует разбиваем по разделителю(пробелу).
+                switch (commandArr[0])
+                {
+                    case ("exit"):
+                        return ;
+
+                    case ("start")://Инициализиция ычислений.
+                        managerRepresentor.StartCalculation();
+                        while (managerRepresentor.CalcultationFlag)
+                        { 
+                            System.Threading.Thread.Sleep(300);//Приостанавливаем работу основного потока пока идут вычисления.
+                        }
+                        break;
+
+                    case ("add_hash"): //Добавляет хеш сумму введенную в консоли(add_hash hash).
+                        if (commandArr.Length == 2 && regex.IsMatch(commandArr[1])) // Проверка валидности хеша.
+                            managerRepresentor.PushNewHash(commandArr[1]);
+                        else
+                            Console.WriteLine("Не правильно введен hash");
+                        break;
+
+                    case ("a"):  //Добавляет хеш суммы из файла(строки файла)
+                        if (commandArr.Length == 2 && File.Exists(commandArr[1]))   //Проверка того, что файл существует.
+                        {
+                            var hashFromFile = File.ReadAllLines(commandArr[1]);    //Чтение файла построчно.
+                            foreach (string hash in hashFromFile)
+                                if(regex.IsMatch(hash)) managerRepresentor.PushNewHash(hash); //Если строка md5, то добавляем в List managerRepresentor.
+                        }
+                        else
+                            Console.WriteLine("Не правильно введен путь к файлу с хешами");
+                        break;
+                }
+
+            }
+        }
+    }
+}
