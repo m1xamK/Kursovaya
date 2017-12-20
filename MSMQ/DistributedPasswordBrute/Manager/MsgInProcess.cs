80873d5c7e0cbd5b8ed44ebf8b147f616cfadefd
lenin155 <lenin.meow@gmail.com>
2017-12-21 02:58:47 +0300

    файлы забыл залить

---
+++ MSMQ/DistributedPasswordBrute/Manager/MsgInProcess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Manager
+{
+	/// <summary>
+	/// структура данных, хранящая информацию о сообщениях, отправленных агентам
+	/// </summary>
+	public class MsgInProcess
+	{
+		//Диапазон, отправленный для обработки агенту в сообщении
+		public KeyValuePair<string, string> Range { get; private set; }
+
+		//Время, в которое было отправленно сообщение агенту
+		public DateTime Time { get; private set; }
+
+		public MsgInProcess(KeyValuePair<string, string> range, DateTime time)
+		{
+			Range = range;
+			Time = time;
+		}
+	}
+}
---
+++ MSMQ/DistributedPasswordBrute/Manager/NextDiapazone.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace Manager
+{
+	public static class NextDiapazone
+	{
+		//алфавит, из которого может состоять хеш
+		public const string Alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+		/// <summary>
+		/// Функция предназначена для нахождения смещения до которого считаем хеш
+		/// </summary>
+		/// <param name="word">Слово, для которого находим смещение</param>
+		/// <returns>Смещение до которого считаем хеш</returns>
+		public static string Get(string word)
+		{
+			const int minLength = 4;
+			const string tail = "000";
+
+			char lastAlphabetSymbol = Alphabet[Alphabet.Length - 1];
+
+			// Первое смещение
+			if (word.Length < minLength)
+				return "1" + tail;
+
+			StringBuilder temp = new StringBuilder(word.Substring(0, word.Length - minLength + 1));
+
+			for (int i = temp.Length - 1; i >= 0; --i)
+			{
+				if (temp[i] != lastAlphabetSymbol)
+				{
+					int index = Alphabet.IndexOf(temp[i]);
+					temp[i] = Alphabet[++index];
+
+					break;
+				}
+
+				temp[i] = '0';
+
+				if (i == 0)
+					temp = new StringBuilder("1").Append(temp);
+			}
+
+			return temp + tail;
+		}
+	}
+}
